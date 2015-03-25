119f9823af96922e976c45f72be73c6f188b0644
Kyle King <KyleKing@users.noreply.github.com>
2015-03-25 18:28:58 -0400

    Explore Factory and Fake Seed Data

--- TeamBIKES/server/seeds/items.js
+++ TeamBIKES/server/seeds/items.js
@@ -1,13 +1,14 @@
 Meteor.startup(function() {
 
   Factory.define('item', Items, {
-    name: function() { return Fake.sentence(); },
+    // name: function() { return Fake.paragraph([5]); },
+    name: function() { return Fake.color(); },
     rating: function() { return _.random(1, 5); }
   });
 
   if (Items.find({}).count() === 0) {
 
-    _(10).times(function(n) {
+    _(15).times(function(n) {
       Factory.create('item');
     });
 
