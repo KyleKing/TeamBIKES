0ce713330e68dc2111c83dd78e3937a10c1ecc8a
Kyle King <KyleKing@users.noreply.github.com>
2015-04-02 00:50:12 -0400

    ...

--- TeamBIKES/client/templates/public/map/map.js
+++ TeamBIKES/client/templates/public/map/map.js
@@ -3,7 +3,7 @@ Template.map.created = function() {
 
     if (Meteor.isClient) {
 
-      // L.Icon.Default.imagePath = 'leaflet/images';
+      L.Icon.Default.imagePath = 'leaflet/images';
 
       var map = new L.Map('map', {
         center: new L.LatLng(38.987701, -76.940989),
@@ -27,42 +27,41 @@ Template.map.created = function() {
       //   attribution: "&copy; <a href=\"http://www.opencyclemap.org\">OpenCycleMap</a>, &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>"
       // }).addTo(map);
 
-      // var zoomControl = L.control.zoom({
-      //   position: 'bottomleft'
-      // });
+      var zoomControl = L.control.zoom({
+        position: 'bottomleft'
+      });
 
-      // map.addControl(zoomControl);
+      map.addControl(zoomControl);
 
-      // // Use Leaflet cluster group plugin
-      // var markers = new L.MarkerClusterGroup();
-      // bikesData = Current.find().fetch();
+      // Use Leaflet cluster group plugin
+      var markers = new L.MarkerClusterGroup();
+      bikesData = Current.find().fetch();
 
-      // // var bikeIconGR = L.icon({
-      // //     iconUrl: 'leaflet/bikes/marker-icon.png',
-      // //     shadowUrl: 'leaflet/bikes/marker-icon.png',
+      var bikeIconGR = L.icon({
+          iconUrl: 'leaflet/bikes/marker-icon.png',
+          shadowUrl: 'leaflet/bikes/marker-icon.png',
 
-      // //     iconSize:     [50, 50], // size of the icon
-      // //     shadowSize:   [0, 0], // size of the shadow
-      // //     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      // //     shadowAnchor: [0, 0],  // the same for the shadow
-      // //     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-      // // });
+          iconSize:     [50, 50], // size of the icon
+          shadowSize:   [0, 0], // size of the shadow
+          iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+          shadowAnchor: [0, 0],  // the same for the shadow
+          popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      });
 
-      // map.addLayer(markers);
+      map.addLayer(markers);
 
-      // // New serial port connection:
-      // var i = bikesData.length - 1;
-      // while (i >= 1) {
-      //   if (!isNaN(bikesData[i].Lat)) {
-      //     // markers.addLayer( new L.Marker(new L.LatLng(bikesData[i].Lat, bikesData[i].Long), {icon: bikeIconGR} ) );
-      //     markers.addLayer( new L.Marker(new L.LatLng(bikesData[i].Lat, bikesData[i].Long) ) );
-      //     console.log(bikesData[i]);
-      //   } else {
-      //     console.log("Bad Bike Location (NaN) - i.e. the current database is empty");
-      //   }
-      //   i--;
-      // }
-      // map.addLayer(markers);
+      // New serial port connection:
+      var i = bikesData.length - 1;
+      while (i >= 1) {
+        if (!isNaN(bikesData[i].Lat)) {
+          markers.addLayer( new L.Marker(new L.LatLng(bikesData[i].Lat, bikesData[i].Long), {icon: bikeIconGR} ) );
+          console.log(bikesData[i]);
+        } else {
+          console.log("Bad Bike Location (NaN) - i.e. the current database is empty");
+        }
+        i--;
+      }
+      map.addLayer(markers);
 
 
       // Creates a red marker with the coffee icon
@@ -70,7 +69,7 @@ Template.map.created = function() {
         icon: 'coffee',
         markerColor: 'red'
       });
-      var marker = L.marker([38.982046, -76.9425723], {icon: redMarker}).addTo(map);
+      var marker = L.marker([38.9820409, -76.94257429999999], {icon: redMarker}).addTo(map);
       // console.log(marker);
 
 
