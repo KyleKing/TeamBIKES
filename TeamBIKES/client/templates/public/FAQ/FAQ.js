736bc22c105e4cf4fa7470f1076a209866955bf5
Kyle King <KyleKing@users.noreply.github.com>
2015-04-01 23:55:32 -0400

    Renamed home to about

--- TeamBIKES/client/templates/public/FAQ/FAQ.js
+++
@@ -1,7 +0,0 @@
-// Template.FAQ.rendered = function () {
-//   $('#nav').affix({
-//       // offset: { top: $('#nav').offset().top }
-//       offset: { top: 100 }
-//   });
-//   // console.log( $('#nav').offset().top);
-// };
