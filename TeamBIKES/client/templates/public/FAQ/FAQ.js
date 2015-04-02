6520915133e3677ba047d3b0f09d954eedbc7ef1
Kyle King <kmking72@gmail.com>
2015-04-01 23:31:39 -0400

    Tried adding affix.js for FAQ section
    Stayed with much more simple .affix

---
+++ TeamBIKES/client/templates/public/FAQ/FAQ.js
@@ -0,0 +1,7 @@
+// Template.FAQ.rendered = function () {
+//   $('#nav').affix({
+//       // offset: { top: $('#nav').offset().top }
+//       offset: { top: 100 }
+//   });
+//   // console.log( $('#nav').offset().top);
+// };
