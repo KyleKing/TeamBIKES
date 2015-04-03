6df04b8fcbff09296ab0c06cdceb1cae519a9e4b
Kyle King <KyleKing@users.noreply.github.com>
2015-04-03 11:33:48 -0400

    Updated and Added DataTable Test

---
+++ TeamBIKES/client/templates/public/testTable.js
@@ -0,0 +1,4 @@
+$('.datatable').dataTable({"sPaginationType": "bs_normal"});
+$('.datatable').dataTable({"sPaginationType": "bs_two_button"});
+$('.datatable').dataTable({"sPaginationType": "bs_four_button"});
+$('.datatable').dataTable({"sPaginationType": "bs_full"});
