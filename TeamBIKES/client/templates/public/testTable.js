1bdfd7a4e59e4ca78560b5467dd61ae881f1d95c
Kyle King <KyleKing@users.noreply.github.com>
2015-04-03 13:51:52 -0400

    Dropdown stopped working with testTable

--- TeamBIKES/client/templates/public/testTable.js
+++
@@ -1,4 +0,0 @@
-$('.datatable').dataTable({"sPaginationType": "bs_normal"});
-$('.datatable').dataTable({"sPaginationType": "bs_two_button"});
-$('.datatable').dataTable({"sPaginationType": "bs_four_button"});
-$('.datatable').dataTable({"sPaginationType": "bs_full"});
