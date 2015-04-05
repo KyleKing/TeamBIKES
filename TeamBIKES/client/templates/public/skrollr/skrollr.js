663d333e266a2249ac8cf913720d660377f1a1ea
Kyle King <KyleKing@users.noreply.github.com>
2015-04-04 21:49:42 -0400

    .... Minor bug fixes

--- TeamBIKES/client/templates/public/skrollr/skrollr.js
+++ TeamBIKES/client/templates/public/skrollr/skrollr.js
@@ -1,3 +1,11 @@
 Template.skrollr.rendered = function () {
-  var s = skrollr.init();
+  ( function( $ ) {
+      // Init Skrollr
+      var s = skrollr.init({
+          render: function(data) {
+              //Debugging - Log the current scroll position.
+              console.log(data.curTop);
+          }
+      });
+  } )( jQuery );
 };
