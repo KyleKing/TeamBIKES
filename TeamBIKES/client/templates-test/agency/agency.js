212c7746e7507d1a79b22143efde208e8563d9bd
Kyle King <KyleKing@users.noreply.github.com>
2015-04-26 20:36:43 -0400

    ... tuning typed.js

--- TeamBIKES/client/templates-test/agency/agency.js
+++ TeamBIKES/client/templates-test/agency/agency.js
@@ -3,7 +3,11 @@ Template.agency.rendered = function () {
   $(function(){
       $(".element-three").typed({
         strings: [" faster", " smarter", " cheaper", " better"],
-        typeSpeed: 20
+        typeSpeed: 150,
+        // time before typing starts
+        startDelay: 300,
+        // time before backspacing
+        backDelay: 500,
       });
   });
 };
