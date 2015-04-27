c23c9bc1fda9977123096d649fb9a10a94db5110
Kyle King <kmking72@gmail.com>
2015-04-27 00:15:30 -0400

    Dark hero. Some may even say gritty

--- TeamBIKES/client/templates-test/agency/agency.js
+++ TeamBIKES/client/templates-test/agency/agency.js
@@ -9,15 +9,12 @@ Template.agency.rendered = function () {
         backDelay: 500,
       });
   });
-};
 
-Template.agency.rendered = function () {
   /*!
    * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
    * Code licensed under the Apache License v2.0.
    * For details, see http://www.apache.org/licenses/LICENSE-2.0.
    */
-
   // jQuery for page scrolling feature - requires jQuery Easing plugin
   $(function() {
       $('a.page-scroll').bind('click', function(event) {
@@ -28,12 +25,10 @@ Template.agency.rendered = function () {
           event.preventDefault();
       });
   });
-
   // Highlight the top nav as scrolling occurs
   $('body').scrollspy({
       target: '.navbar-fixed-top'
   });
-
   // Closes the Responsive Menu on Menu Item Click
   $('.navbar-collapse ul li a').click(function() {
       $('.navbar-toggle:visible').click();
