d8e9a041402015e257ceb9ae4804148b7bc64ed1
Kyle King <kmking72@gmail.com>
2015-06-05 19:34:50 -0400

    Purged app to start major edits

--- TeamBIKES/client/templates-test/snapSVG/snapsvgdraw.js
+++
@@ -1,5 +0,0 @@
-// snapsvgdraw is a template with nothing in it. this draws a 800 x 600 canvas for drawing.
-Template.snapsvgdraw.rendered = function () {
-  "use strict";
-  Meteor.setTimeout(function () {var s = new Snap(800, 600), bigCircle = s.circle(150, 150, 100); }, 1000);
-};
