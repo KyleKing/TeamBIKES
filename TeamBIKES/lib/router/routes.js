57166bb7279856fb74e341fb7af16802e3c00bcc
Kyle King <KyleKing@users.noreply.github.com>
2015-06-05 19:34:50 -0400

    Purged app to start major edits

--- TeamBIKES/lib/router/routes.js
+++ TeamBIKES/lib/router/routes.js
@@ -1,51 +1,15 @@
-// Global Styles
+// Global Route Configuration
 Router.configure({
   loadingTemplate: 'loading',
-  notFoundTemplate: 'notFound'
+  notFoundTemplate: 'notFound',
+  layoutTemplate: 'appLayout'
 });
 
-// Do these even work?
+// Plugins
 Router.plugin('dataNotFound', {dataNotFoundTemplate: 'notFound'});
-
-
-/*********************************************/
-/*   Public                                                         */
-/********************************************/
-Router.route('/', {
-  name: 'about',
-});
-
-Router.route('/contact', {
-  layoutTemplate: 'appLayout'
-});
-
-Router.route('/map', {
-  // subscriptions: function() {
-  //   // returning a subscription handle or an array of subscription handles
-  //   // adds them to the wait list.
-  //   return Meteor.subscribe('currentData');
-  // },
-  layoutTemplate: 'fullLayout',
-});
-
-/*********************************************/
-/*   Make sure sign in only, but student role          */
-/********************************************/
-Router.route('/student', {
-  layoutTemplate: 'appLayout'
-});
-
-
-/*********************************************/
-/*   The Secuirty Barrier in between          */
-/********************************************/
 Router.plugin('ensureSignedIn', {
-  // Array of pages only visible to users
-  only: ['dashboard']
-  // Array of pages not secured
-  // except: ['about', 'FAQ', 'map', 'atSignIn', 'atSignUp', 'atForgotPassword', 'onePageScroll', 'skrollr', 'progress', 'home']
+  except: ['about', 'atSignIn', 'atSignUp', 'atForgotPassword', 'progress']
 });
-
 
 // Sign out and go to home page with route control
 // Useful, from: http://stackoverflow.com/questions/27743660/proper-syntax-for-iron-routers-router-route
@@ -58,98 +22,75 @@ Router.route('/sign-out', function() { }, {
   onAfterAction: function () { Router.go('/'); }
 });
 
-
-// /*********************************************/
-// /*   Private          */
-// /********************************************/
-// Router.route('/dashboard', {
-//   controller: 'AppController'
-// });
-
-// Router.route('/users', {
-// });
-
-// Router.route('/charts', {
-//   name: 'chartsAdmin',
-//   controller: 'AppController'
-// });
-
-Router.route('/RFIDlayout', {
-  layoutTemplate: 'appLayout'
+/*   Public */
+Router.route('/', {
+  name: 'about'
 });
 
-// Router.route('/mechanicView', {
-//   controller: 'AppController'
-// });
+/* User */
+Router.route('/student');
+Router.route('/map', {
+  layoutTemplate: 'fullLayout'
+});
 
-// Router.route('/mechmap', {
-//   controller: 'MapController'
-// });
+/* Administrator */
+Router.route('/dashboard');
+Router.route('/users');
 
-// Router.route('/timeseries', {
-//   controller: 'AppController'
-// });
+Router.route('/charts', {
+  name: 'chartsAdmin'
+});
 
-// Router.route('/admin2layoutAllData', {
-//   controller: 'AppController',
-//    waitOn: function () {
-//      return Meteor.subscribe("TestUsersData");
-//    },
-//   data: function() {
-// 	 return TestUsers.find({},{limit: 20});
-//   }
-// });
+Router.route('/RFIDlayout');
+Router.route('/mechanicView');
+Router.route('/mechmap');
+Router.route('/timeseries');
 
-// Router.route('/admin2layoutBikeData/:_id', {
-//   name: 'admin2layoutBikeData',
-//   controller: 'AppController',
-//   // // Note: this.params._id is shorthand for {_id: id}
-//   waitOn: function () {
-// 	return Meteor.subscribe("TestUsersData");
-//   },
-//   data: function() { return TestUsers.findOne(this.params._id); }
-// });
+Router.route('/admin2layoutAllData', {
+  waitOn: function () {
+   return Meteor.subscribe("TestUsersData");
+  },
+  data: function() {
+	 return TestUsers.find({},{limit: 20});
+  }
+});
 
-// Router.route('/admin3layoutAllData', {
-//   name: 'admin3layoutAllData',
-//   controller: 'AppController',
-//   waitOn: function () {
-//     return Meteor.subscribe("TimeSeriesData");
-//   },
-//   data: function() {
-// 	return TimeSeries.find({},{limit: 20});
-//   }
-// });
+Router.route('/admin2layoutBikeData/:_id', {
+  name: 'admin2layoutBikeData',
+  // // Note: this.params._id is shorthand for {_id: id}
+  waitOn: function () {
+	return Meteor.subscribe("TestUsersData");
+  },
+  data: function() { return TestUsers.findOne(this.params._id); }
+});
 
-// Router.route('/admin3layoutBikeData/:_id', {
-//   name: 'admin3layoutBikeData',
-//   controller: 'AppController',
-//   waitOn: function () {
-// 	return Meteor.subscribe("TimeSeriesData");
-//   },
-//   data: function() { return TimeSeries.findOne(this.params._id); }
-// });
+Router.route('/admin3layoutAllData', {
+  name: 'admin3layoutAllData',
+  waitOn: function () {
+    return Meteor.subscribe("TimeSeriesData");
+  },
+  data: function() {
+	return TimeSeries.find({},{limit: 20});
+  }
+});
 
-// /*********************************************/
-// /*   WIP - Test Routes          */
-// /********************************************/
+Router.route('/admin3layoutBikeData/:_id', {
+  name: 'admin3layoutBikeData',
+  waitOn: function () {
+	return Meteor.subscribe("TimeSeriesData");
+  },
+  data: function() { return TimeSeries.findOne(this.params._id); }
+});
 
-// Router.route('/progress', {
-//   controller: 'AppController'
-// });
-
-// Router.route('/agency');
-
+/*********************************************/
+/*   WIP - Test Routes                       */
+/********************************************/
+Router.route('/progress');
+Router.route('/agency');
 
 // /*********************************************/
-// /*   Remove Below content for Production          */
+// /*   Remove Below content for Production     */
 // /********************************************/
-
 // // For testing loading page with no wait time
 // // Todo: Integrate more seamlessly into app
 // Router.route('/loading');
-
-// // Router.route('/items_0', {
-// //   name: 'items',
-// //   controller: 'AppController'
-// // });
