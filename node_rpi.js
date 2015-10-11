19336da5a4dff29fab0b31c76128172e6780501a
Kyle King <KyleKing@users.noreply.github.com>
2015-10-11 15:30:05 -0400

    Configured slimmer node client

--- node_rpi.js
+++ node_rpi.js
@@ -1,7 +1,7 @@
 // Name serial port - there should be a smarter way to do this, but this seems easiest
 // Using terminal, identify with:
 // ls /dev/cu.*
-// ls /dev/tty.usbserial-* # for zigbee
+// ls /dev/cu.usbserial-* # for zigbee
 // ls /dev/cu.usbmodem* # for arduino
 
 var currentPort = "/dev/tty.usbserial-AH016D5G"; // Direct left Zigbee
@@ -10,26 +10,10 @@ var DDPClient = require("ddp");
 var moment = require('moment');
 moment().format();
 
-var util = require('util');
-var SerialPort = require('serialport').SerialPort;
+var xbee = require("xbee");
 
-var xbee_api = require('xbee-api');
-var C = xbee_api.constants;
-var xbeeAPI = new xbee_api.XBeeAPI({
-  api_mode: 2
-});
-
-var lastID = '';
-
-// Config
+// Connect to Meteor
 var ddpclient = new DDPClient({
-  // // Remote
-  // // Source: https://github.com/oortcloud/node-ddp-client/issues/21
-  // host: "redbarbikes.com",
-  // port: 80,
-  // auto_reconnect: true
-
-  // Local
   host: "localhost",
   port: 3000,
   /* optional: */
@@ -40,130 +24,47 @@ var ddpclient = new DDPClient({
   use_ssl_strict: true, //Set to false if you have root ca trouble.
   maintain_collections: true //Set to false to maintain your own collections.
 });
-// Connect to Meteor
+
 ddpclient.connect(function(error) {
   // Error Checking
-  if (error) throw error;
+  if (error) {
+    console.log('DDP connection error!');
+    return;
+  }
   console.log('connected to Meteor!');
-  // // Login - Note may not work?
-  // login(ddpclient,
-  //   {  // Options below are the defaults
-  //      env: 'METEOR_TOKEN',  // Name of an environment variable to check for a
-  //                            // token. If a token is found and is good,
-  //                            // authentication will require no user interaction.
-  //      method: 'email',    // Login method: account, email, username or token
-  //      account: 'admin@example.com',        // Prompt for account info by default
-  //      pass: 'password',           // Prompt for password by default
-  //      retry: 2,             // Number of login attempts to make
-  //      plaintext: false      // Do not fallback to plaintext password compatibility
-  //                            // for older non-bcrypt accounts
-  //   },
-  //   function (error, userInfo) {
-  //     if (error) {
-  //       // Something went wrong...
-  //       console.log('error');
-  //     } else {
-  //       // We are now logged in, with userInfo.token as our session auth token.
-  //       token = userInfo.token;
-  //     }
-  //   }
-  // );
 
   // Configure serial port
+  var serialport = require("serialport");
+  var SerialPort = serialport.SerialPort; // localize object constructor
   var serialPort = new SerialPort(currentPort, {
     // baudrate: 115200,
     baudrate: 9600,
-    parser: xbeeAPI.rawParser()
+    // look for return and newline at the end of each data packet:
+    // parser: serialport.parsers.readline("\r\n")
+    // look for ; character to signify end of line
+    // parser: serialport.parsers.readline(";")
+    parser: xbee.packetParser()
   });
 
-  // SerialPort events - trigger specific functions upon specific events
-  serialPort.on('open', function() {
-    console.log('SerialPort is open with rate of ' + serialPort.options.baudRate);
-  });
+  function showPortOpen() {
+    console.log('port open. Data rate: ' + serialPort.options.baudRate);
+  }
 
-  // All frames parsed by the XBee will be emitted here
-  xbeeAPI.on("frame_object", function(frame) {
-    if (frame) {
-      frame = frame.trim();
-      console.log('--------------------');
-      if (frame.data === undefined) {
-        if (frame.deliveryStatus === 0) {
-          console.log('>> Data was delivered!');
-          console.log(frame);
-        } else {
-          console.log('>> No data in frame received');
-          console.log('   Data was not received');
-          console.log('   ' + frame);
-        }
-        console.log(' ');
-      } else if (frame.data != lastID) {
+  function saveLatestData(data) {
+    console.log('xbee data received:', data.data);
+  }
 
-        data = frame.data.toString();
-        console.log(">> Serial: " + data);
-        // console.log("OBJ> " + util.inspect(frame));
-
-        var array = data.split(','); // CSV Data Parse:
-        // array.push( (new Date()).getTime() );
-        var time = (new Date()).getTime()
-        var dataSet = {
-          USER_ID: array[0],
-          LATITUDE: array[1],
-          LONGITUDE: array[2],
-          LOCKSTATEE: array[3],
-          Module_ID: array[4],
-          TIMESTAMP: time
-        };
-
-        lastID = array[0];
-
-        // Call Meteor actions with "dataSet"
-        ddpclient.call('RFIDStreamData', [dataSet], function(err, result) {
-          console.log('>> Sent to Meteor: ' + array);
-
-          // // P-J's Suggestion
-          // var CryptoJS = require("crypto-js");
-          // var info = { message: "This is my message !", key: "Dino" };
-
-          // var encrypted = CryptoJS.AES.encrypt(info.message, info.key, {
-          //     mode: CryptoJS.mode.CBC,
-          //     padding: CryptoJS.pad.Pkcs7
-          // });
-          // var decrypted = CryptoJS.AES.decrypt(result, info.key, {
-          //     mode: CryptoJS.mode.CBC,
-          //     padding: CryptoJS.pad.Pkcs7
-          // });
-          // // console.log(encrypted.toString());
-          // console.log('Decrypted result: '+ decrypted.toString(CryptoJS.enc.Utf8));
-
-          console.log('Result: ' + result);
-          if (result != undefined) {
-            var frame_obj = {
-              type: 0x10, // xbee_api.constants.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST
-              id: 0x01, // optional, nextFrameId() is called per default
-              // destination64: "0013A20040B7B31F", // End
-              destination64: "0013A20040C5F8BA", // R-Duck
-              // destination64: "0013A20040B90B95", // R-Whip
-              destination16: "fffe", // optional, "fffe" is default
-              broadcastRadius: 0x00, // optional, 0x00 is default
-              options: 0x00, // optional, 0x00 is default
-              data: result // Can either be string or byte array.
-            };
-            serialPort.write(xbeeAPI.buildFrame(frame_obj));
-            console.log('   Frame sent to specific xbee');
-            console.log(xbeeAPI.buildFrame(frame_obj));
-          }
-          console.log(' ');
-        });
-      } else {
-        console.log('Ignoring an incoming frame');
-      }
-    }
-  });
-
-  serialPort.on('close', function() {
+  // Error Checking
+  function showPortClose() {
     console.log('port closed.');
-  });
-  serialPort.on('error', function(error) {
+  }
+  function showError(error) {
     console.log('Serial port error: ' + error);
-  });
+  }
+
+  // Node events - trigger specific functions upon specific events
+  serialPort.on('open', showPortOpen);
+  serialPort.on('data', saveLatestData);
+  serialPort.on('close', showPortClose);
+  serialPort.on('error', showError);
 });
