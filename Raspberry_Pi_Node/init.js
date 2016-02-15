51d38f9c4a304da8528d56df2fb6ac8e58f507bc
Kyle King <KyleKing@users.noreply.github.com>
2016-02-15 13:56:45 -0500

    Reinstated a node app instead of a meteor for rpi

---
+++ Raspberry_Pi_Node/init.js
@@ -0,0 +1,195 @@
+// Connects to a serial device (Arduino or XBee/ZIgbee) automatically
+// Then reacts to incoming serial data in a csv format with ';' or
+//    XBee frames respectively
+// Collected data is parsed into a specific array that is then sent over a
+//    DDP connection to a locally hosted meteor application by calling the
+//    RFIDStream method
+
+// TODO
+// Currently installed:
+// npm install serialport util moment cli-color ddp xbee-api --save
+// Still need:
+// npm install ddp-login --save
+
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+var util = require('util');
+
+var moment = require('moment');
+moment().format();
+
+var clc = require('cli-color');
+var info = clc.blue;
+var h1 = info.bold;
+var warn = clc.yellow;
+
+var DDPClient = require('ddp');
+var xbee_api = require('xbee-api');
+var C = xbee_api.constants;
+
+xbeeAPI = new xbee_api.XBeeAPI({
+  api_mode: 2
+});
+
+var lastID = '';
+var lastModule_ID = '';
+
+var currentHost = ['localhost', 3000];
+// var currentHost = ['somewebsite', 3000];
+
+var ddpclient = new DDPClient({
+  host: currentHost[0],
+  port: currentHost[1],
+  auto_reconnect: true,
+  auto_reconnect_timer: 500,
+  use_ejson: true,
+  use_ssl: false,
+  use_ssl_strict: true,
+  maintain_collections: true
+});
+
+function parseInput(frame) {
+  data = frame.data.toString();
+  console.log('* Converted To: ' + data);
+  array = data.split(',');
+  return dataSet = {
+    USER_ID: array[0],
+    LATITUDE: array[1],
+    LONGITUDE: array[2],
+    LOCKSTATEE: array[3],
+    Module_ID: array[4],
+    TIMESTAMP: (new Date).getTime()
+  };
+}
+
+
+ddpclient.connect(function(error) {
+  if (error) {
+    throw error;
+  } else {
+    console.log(info('Connected to Meteor!'));
+  }
+  serialport.list(function(err, ports) {
+    return ports.forEach(function(port) {
+      console.log(port);
+      console.log(port.comName + '\n');
+
+      // See rpi.coffee in Raspberry_Pi_Meteor for example Crypto.js script
+
+      // Figure out active Serial Port - Automatic Method
+      // Using terminal, identify with:
+      // ls /dev/cu.*
+      if (port.comName.match(/\/dev\/tty.usbserial-*/)) {
+        xbee_createSerial(port.comName);
+      }
+      if (port.comName.match(/\/dev\/cu.usbmodem*/)) {
+        arduino_createSerial(port.comName);
+      }
+    });
+  });
+});
+
+arduino_createSerial = function(currentPort) {
+  console.log(warn('Created New Serial Port Connection to ' +
+    currentPort));
+  var arduino_serialPort = new SerialPort(currentPort, {
+    baudrate: 9600,
+    parser: serialport.parsers.readline("\n")
+  });
+  arduino_serialPort.on('open', function() {
+    return console.log('SerialPort is open with rate of ' +
+      arduino_serialPort.options.baudRate);
+  });
+  arduino_serialPort.on('data', function(frame) {
+    var array, data, dataSet;
+    console.log(h1('----------*----------'));
+    console.log('* Received Frame:');
+    console.log(frame);
+    dataSet = parseInput(frame);
+    return ddpclient.call('RFIDStreamData', [dataSet], function(err, res) {
+      console.log('>> Sent to Meteor: ' + array);
+      if (res !== void 0 && res.data !== void 0) {
+        if (res.Address === void 0) {
+          console.log(warn("Warning: BROADCASTING TO ALL XBEE's, " +
+            "I hope you know what you are doing."));
+        }
+        console.log('>> Writing ' + res.data);
+        arduino_serialPort.write(res.data);
+      }
+    });
+  });
+  arduino_serialPort.on('close', function() {
+    return console.log(warn('port closed.'));
+  });
+  return arduino_serialPort.on('error', function(error) {
+    return console.log(warn('Serial port error: ' + error));
+  });
+};
+
+xbee_createSerial = function(currentPort) {
+  console.log(warn('Created New Serial Port Connection to ' +
+    currentPort));
+  var xbee_serialPort = new SerialPort(currentPort, {
+    baudrate: 9600,
+    parser: xbeeAPI.rawParser()
+  });
+  xbee_serialPort.on('open', function() {
+    return console.log('SerialPort is open with rate of ' +
+      xbee_serialPort.options.baudRate);
+  });
+  xbeeAPI.on('frame_object', function(frame) {
+    var array, data, dataSet;
+    console.log(h1('----------*----------'));
+    console.log('* Received Frame:');
+    console.log(frame);
+    if (frame.data === undefined) {
+      if (frame.deliveryStatus === 0) {
+        console.log(info('>> Data was delivered!'));
+        return console.log(info(frame + '\n'));
+      } else {
+        console.log(warn('>> No data in frame received'));
+        console.log(warn('   Data was not received'));
+        return console.log(warn('   ' + frame + '\n'));
+      }
+    } else {
+      dataSet = parseInput(frame);
+      // Not sure if this is necessary:
+      // if dataSet.USER_ID isnt lastID
+      if (true === true && false === false) {
+        lastID = dataSet.USER_ID;
+        return ddpclient.call('RFIDStreamData', [dataSet], function(err, res) {
+          var frame_obj;
+          console.log('>> Sent to Meteor: ' + array);
+          if (res !== void 0 && res.data !== void 0) {
+            if (res.Address === void 0) {
+              console.log(warn("Warning: BROADCASTING TO ALL XBEE's, " +
+                "I hope you know what you are doing."));
+            }
+            frame_obj = {
+              type: 0x10,
+              id: 0x01,
+              destination64: res.Address,
+              destination16: 'fffe',
+              broadcastRadius: 0x00,
+              options: 0x00,
+              data: res.data
+            };
+            xbee_serialPort.write(xbeeAPI.buildFrame(frame_obj));
+            console.log(h1('Frame sent to specific xbee: ' + res.Address));
+            console.log(h1(xbeeAPI.buildFrame(frame_obj)));
+          }
+          console.log('----------!----------');
+          return console.log('');
+        });
+      } else {
+        return console.log('Received a duplicate frame');
+      }
+    }
+  });
+  xbee_serialPort.on('close', function() {
+    return console.log(warn('port closed.'));
+  });
+  return xbee_serialPort.on('error', function(error) {
+    return console.log(warn('Serial port error: ' + error));
+  });
+};
---
+++ Raspberry_Pi_Node/lcd.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import time
+import Adafruit_CharLCD as LCD
+
+# Raspberry Pi pin configuration:
+lcd_rs = 24  # 27 - yellow
+lcd_en = 25  # 22 - green
+lcd_d4 = 12  # 25 - blue
+lcd_d5 = 13  # 24 - violet
+lcd_d6 = 16  # 23 - grey
+lcd_d7 = 19  # 27 - white
+lcd_backlight = 4
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows = 2
+
+# Alternatively specify a 20x4 LCD.
+# lcd_columns = 20
+# lcd_rows = 4
+
+# Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
+
+def full_message(message):
+    lcd.clear()
+    # prints "some_var is smaller than 10"
+    if "\n" not in message:
+        if len(message) < lcd_columns:
+            # Normal Method
+            lcd.message(message)
+            time.sleep(1.0)
+            return False
+        elif len(message) <= 2*lcd_columns:
+            # Split into two rows
+            # TODO make break at space, if possible
+            first_line = message[:lcd_columns]
+            second_line = message[lcd_columns:]
+            lcd.message(first_line + "\n" + second_line)
+            time.sleep(1.0)
+            return True
+        elif len(message) > 2*lcd_columns:
+            # Brute force the message
+            # FIXME the message seems to be split and breaks scroll
+            lcd.message(message)
+            for i in range(len(message)-lcd_columns):
+                time.sleep(0.5)
+                lcd.move_left()
+            time.sleep(1.0)
+            return True
+        else:
+            print "Something is wrong with the string length"
+            return False
+    else:
+        lcd.message(message)
+        return True
+
+full_message('Hello\nworld!')
+time.sleep(4.0)
